375c18b8bfa70e4a840e4c95631bffc84632c600
zeinamagdy <zainabmagdy8@gmail.com>
2020-05-28 15:32:34 +0200

    Hide state from debugetool

--- src/index.js
+++ src/index.js
@@ -17,7 +17,8 @@ const rootReducer = combineReducers({
     orderReducer: orderReducer,
     authReducer: authReducer
 })
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// access env variable (node module-> react-scripts ->config) to hide state from devtool ex
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
